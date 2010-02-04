ac7c3f05e5e3f585ec0014a832b3f3ccca2150da
Jason Dillon <jason@planet57.com>
2010-02-04 16:58:52 +0700

    Re-implemented command resolver using a tree, added initial group support

--- mvnsh-core/src/main/java/org/sonatype/maven/shell/Main.java
+++ mvnsh-core/src/main/java/org/sonatype/maven/shell/Main.java
@@ -15,7 +15,7 @@ import com.google.inject.Stage;
 import com.google.inject.name.Names;
 import org.sonatype.gshell.MainSupport;
 import org.sonatype.gshell.branding.Branding;
-import org.sonatype.gshell.command.CommandRegistrar;
+import org.sonatype.gshell.command.registry.CommandRegistrar;
 import org.sonatype.gshell.command.IO;
 import org.sonatype.gshell.console.ConsoleErrorHandler;
 import org.sonatype.gshell.console.ConsolePrompt;
