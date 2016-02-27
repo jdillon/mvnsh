7bd3fa2d864f5689f8dbfd9ef84d794b1182e8af
Jason Dillon <jason@planet57.com>
2016-02-27 14:23:43 -0800

    helper

---
+++ set-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2009-present Sonatype, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# and Apache License v2.0 which accompanies this distribution.
+# The Eclipse Public License is available at
+#   http://www.eclipse.org/legal/epl-v10.html
+# The Apache License v2.0 is available at
+#   http://www.apache.org/licenses/LICENSE-2.0.html
+# You may elect to redistribute this code under either of these licenses.
+#
+
+dirname=`dirname $0`
+dirname=`cd "$dirname" && pwd`
+cd "$dirname"
+
+# This will update all pom.xml files to use version specified as first parameter of this script
+
+newVersion=$1
+if [ -z "$newVersion" ]; then
+    echo "usage: `basename $0` <new-version>"
+    exit 1
+fi
+
+exec mvn org.eclipse.tycho:tycho-versions-plugin:0.23.1:set-version \
+    -Dtycho.mode=maven \
+    -Dartifacts=mvnsh \
+    -DnewVersion="$newVersion"
