25ab9b181672c0248f0307644cd30164ae063b28
Jason Dillon <jason@planet57.com>
2017-04-20 14:26:07 -0700

    Use gshell-testhardness module

--- mvnsh-commands/mvnsh-maven/src/test/java/com/planet57/maven/shell/commands/maven/MavenCommandTest.java
+++ mvnsh-commands/mvnsh-maven/src/test/java/com/planet57/maven/shell/commands/maven/MavenCommandTest.java
@@ -17,7 +17,7 @@ package com.planet57.maven.shell.commands.maven;
 
 import java.io.File;
 
-import com.planet57.gshell.command.CommandTestSupport;
+import com.planet57.gshell.testharness.CommandTestSupport;
 import org.junit.Ignore;
 import org.junit.Test;
 
