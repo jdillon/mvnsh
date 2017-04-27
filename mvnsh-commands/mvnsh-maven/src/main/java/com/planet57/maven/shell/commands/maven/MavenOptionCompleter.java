624ccfe75d9061ff3f2832a04d34214541d455dd
Jason Dillon <jason@planet57.com>
2017-04-26 17:54:00 -0700

    more complete options from maven 3.5.0

--- mvnsh-commands/mvnsh-maven/src/main/java/com/planet57/maven/shell/commands/maven/MavenOptionCompleter.java
+++ mvnsh-commands/mvnsh-maven/src/main/java/com/planet57/maven/shell/commands/maven/MavenOptionCompleter.java
@@ -41,11 +41,45 @@ public class MavenOptionCompleter
   private final StringsCompleter2 delegate = new StringsCompleter2();
 
   public MavenOptionCompleter() {
+    // TODO: adjust for options with arguments; commented out for now
+
     delegate.addAll(
       "-am", "--also-make",
+      "-amd", "--also-make-dependents",
       "-B", "--batch-mode",
+      // "-b", "--builder",
+      "-C", "--strict-checksums",
+      "-c", "--lax-checksums",
+      "-cpu", "--check-plugin-updates",
+      // "-D", "--define",
       "-e", "--errors",
+      // "-emp", "--encrypt-master-password",
+      // "-ep", "--encrypt-password",
+      // "-f", "--file",
+      "-fae", "--fail-at-end",
+      "-ff", "--fail-fast",
+      "-fn", "--fail-never",
+      // "-gs", "--global-settings",
+      // "-gt", "--global-toolchains",
+      "-h", "--help",
+      // "-l", "--log-file",
+      "-llr", "--legacy-local-repository",
       "-N", "--non-recursive",
+      "-npr", "--no-plugin-registry",
+      "-npu", "--no-plugin-updates",
+      "-nsu", "--no-snapshot-updates",
+      "-o", "--offline",
+      // "-P", "--activate-profiles",
+      // "-pl", "--projects",
+      "-q", "--quiet",
+      // "-rf", "--resume-from",
+      // "-s", "--settings",
+      // "-t", "--toolchains",
+      // "-T", "--threads",
+      "-U", "--update-snapshots",
+      "-up", "--update-plugins",
+      "-v", "--version",
+      "-V", "--show-version",
       "-X", "--debug"
     );
   }
